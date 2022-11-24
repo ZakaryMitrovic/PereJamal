29564a9af641efbf0bc2f9bbec1398f279dbf918
Mathis Leblanc <1949383@cstj.qc.ca>
2022-11-24 15:15:21 -0500

    Rajout des mats

--- Assets/Scripts/Candle.cs
+++ Assets/Scripts/Candle.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class Candle : MonoBehaviour
 {
     [SerializeField] private GameObject _fire;
+    [SerializeField] private Material _waxAllumer;
 
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Fire"))
         {
             _fire.SetActive(true);
+            Material[] mats = GetComponent<MeshRenderer>().materials;
+            mats[0] = _waxAllumer;
+            GetComponent<MeshRenderer>().materials = mats;
         }
     }
 }
