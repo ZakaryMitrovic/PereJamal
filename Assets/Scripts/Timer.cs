e24fa33be79ea3aab0042cec1af38cd49c4dc67f
ZakaryMitrovic <1944315@cstj.qc.ca>
2022-12-08 23:01:17 -0500

    Ajout du chapeau de police

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
@@ -27,7 +28,7 @@ public class Timer : MonoBehaviour
             _time.text = min + ":" + seconde;
         }
         if(temps <= 0){
-            
+            SceneManager.LoadScene("Mort");
         }
     }
 }
