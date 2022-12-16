34a54187345cfbcd9c3c3dcb07b7771169a75138
Mathis Leblanc <1949383@cstj.qc.ca>
2022-12-16 12:44:58 -0500

    Correction

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -10,7 +10,6 @@ public class Lever : MonoBehaviour
     [SerializeField] private AudioSource _audioStatue;
     [SerializeField] private AudioClip _sonLever;
     [SerializeField] private GameObject _Statue;
-    [SerializeField] private Statue _statues;
 
     public XRDirectInteractor rHand, lHand;
     public GameObject _leftHand;
@@ -33,12 +32,8 @@ public class Lever : MonoBehaviour
     void Update()
     {
         if(hJ.angle >= 90){
-            if(_statues.doorIsOpen == false) {
-                _Statue.transform.Rotate(0,0,1f);
-                _audioStatue.enabled = true;
-            } else {
-                _audioStatue.enabled = false;
-            }
+            _Statue.transform.Rotate(0,0,1f);
+            _audioStatue.enabled = true;
             if(dejaActiver == false){
                 dejaActiver = true;
                 _audio.PlayOneShot(_sonLever);
